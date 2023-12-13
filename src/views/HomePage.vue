083952fb9aa6ed14ad6914bce30ad30231142fd1
DBulacio <daniel.bulacio@hotmail.com>
2023-12-13 00:15:31 -0300

    starting project and adding infinite scrolling

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,61 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+  <ion-content>
+    <ion-list>
+      <ion-item v-for="(item, index) in items" :key="index">
+        <ion-avatar slot="start">
+          <img :src="'https://picsum.photos/80/80?random=' + index" alt="avatar" />
+        </ion-avatar>
+        <ion-label>{{ item }}</ion-label>
+      </ion-item>
+    </ion-list>
+    <ion-infinite-scroll @ionInfinite="ionInfinite">
+      <ion-infinite-scroll-content></ion-infinite-scroll-content>
+    </ion-infinite-scroll>
+  </ion-content>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import {
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonImg,
+  IonLabel,
+} from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonImg,
+    IonLabel,
+  },
+  setup() {
+    const items = ref([]);
+
+    const generateItems = () => {
+      const count = items.value.length + 1;
+      for (let i = 0; i < 50; i++) {
+        items.value.push(`Item ${count + i}`);
+      }
+    };
+
+    const ionInfinite = (ev) => {
+      generateItems();
+      setTimeout(() => ev.target.complete(), 500);
+    };
+
+    generateItems();
+
+    return { ionInfinite, items };
+  },
+});
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
